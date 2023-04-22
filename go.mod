4f2e4d20082cca3dd5aebc559278e230ae06aaee
ningenMe <amohkuru@gmail.com>
2023-04-22 23:54:31 +0900

    [fix]

--- go.mod
+++ go.mod
@@ -4,14 +4,9 @@ go 1.19
 
 require (
 	github.com/bufbuild/connect-go v1.6.0
+	github.com/rs/cors v1.9.0
 	golang.org/x/net v0.9.0
-	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
 )
 
-require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
-)
+require golang.org/x/text v0.9.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ningenMe/miiko-api/pkg/application"
 	"github.com/ningenMe/miiko-api/proto/gen_go/v1/miikov1connect"
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"net/http"
@@ -26,7 +27,13 @@ func main() {
 	mux := http.NewServeMux()
 	path, handler := miikov1connect.NewMiikoServiceHandler(miiko)
 	mux.Handle(path, handler)
-	err := http.ListenAndServe("localhost:8081", h2c.NewHandler(mux, &http2.Server{}))
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"https://ningenme.net", "http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	}).Handler(h2c.NewHandler(mux, &http2.Server{}))
+	err := http.ListenAndServe("localhost:8081", corsHandler)
 	if err != nil {
 		return
 	}
