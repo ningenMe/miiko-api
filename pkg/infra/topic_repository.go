78b330ce5879e1ab0bffe8bc1b8b5fd86bcdf7fc
ningenMe <amohkuru@gmail.com>
2023-05-01 00:40:02 +0900

    [fix]

--- pkg/infra/topic_repository.go
+++ pkg/infra/topic_repository.go
@@ -56,27 +56,3 @@ func getProblemWithListByTopicId(topicId string) []*ProblemDto {
 
 	return list
 }
-
-func getProblemWithListByTopicId(topicId string) []*ProblemDto {
-	var list []*ProblemDto
-
-	rows, err := ComproMysql.NamedQuery(
-		`SELECT p.problem_id, url, problem_display_name, estimation FROM problem AS p JOIN relation_topic_problem AS rtp ON p.problem_id = rtp.problem_id WHERE rtp.topic_id = :topicId ORDER BY estimation`,
-		map[string]interface{}{
-			"topicId": topicId,
-		})
-	if err != nil {
-		fmt.Println(err)
-		return list
-	}
-
-	for rows.Next() {
-		c := &ProblemDto{}
-		if err = rows.StructScan(c); err != nil {
-			fmt.Println(err)
-		}
-		list = append(list, c)
-	}
-
-	return list
-}
